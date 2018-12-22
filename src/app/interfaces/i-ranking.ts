054978518d5e92b6459f554d45bd85c49ecb7911
Ersen Başaran Şen <ersen.sen@gmail.com>
2018-12-22 13:46:28 +0300

    interfaces and mock schedule data added

---
+++ src/app/interfaces/i-ranking.ts
@@ -0,0 +1,9 @@
+/** Rankings interface */
+export interface IRanking {
+    TeamName: string,
+    GamesPlayed: number,
+    Wins: number,
+    Ties: number,
+    GoalsFor: number,
+    GoalsAgainst: number
+}
---
+++ src/app/interfaces/i-schedule.ts
@@ -0,0 +1,11 @@
+/** Schedule interface */
+export interface ISchedule {
+    id: number,
+    PlayingDate: Date,
+    HomeTeam: string,
+    AwayTeam: string,
+    HomeScore: number,
+    AwayScore: number,
+    RefName: string,
+    notes?: string
+}
